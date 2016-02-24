005c6fb3840d75decd3972cc0a42c5f1066062d4
yixiaoluo <xiaoluo.yxl@alibaba-inc.com>
2016-02-24 19:33:15 +0800

    support config in storyboard and add auto layout support

--- Class/TIVTextView.swift
+++ Class/TIVTextView.swift
@@ -9,52 +9,71 @@
 import UIKit
 
 protocol TIVTextViewDelegate : class{
-    var maxAllowCharactersCount:Int {get}//允许输入字符的最大个数
+    var maxAllowCharactersCount:Int {get}//max character count that allow
     
-    func textView(didBeginEditing textView:TIVTextView)//开始输入
-    func textView(didEndEditing textView:TIVTextView)//结束输入
+    func textView(didBeginEditing textView:TIVTextView)//text input did begin
+    func textView(didEndEditing textView:TIVTextView)//text input did end
     
-    func textView(shouldEndEditing  textView:TIVTextView) -> Bool//键盘是否该收起
-    func textView(didChanged textView:TIVTextView) -> (String, Bool)//文字发生变化时，调用方返回提示文字及是否超过文字个数限制。此方法被放入了子线程调用，调用方不用担心主线程被卡顿
+    func textView(shouldEndEditing  textView:TIVTextView) -> Bool//should end editing when user press the Reture key?
+    func textView(didChanged textView:TIVTextView) -> (String, Bool)//the caller should offer the tip and whether exceeded the max character count. this method will be operate in a serial background queue, so, it will not block the main thread.
 }
 
 class TIVTextView: UIView {
     
-    unowned var delegate:TIVTextViewDelegate
-    let calQueue:dispatch_queue_t = dispatch_queue_create("com.alibaba.TIVTextView", DISPATCH_QUEUE_SERIAL)
+    weak var delegate:TIVTextViewDelegate?
+    let calQueue:dispatch_queue_t = dispatch_queue_create("com.alibaba.TIVTextView", DISPATCH_QUEUE_SERIAL)//text length calculate will be excute in this queue to avoid potential block in main thread
 
-    //输入状态
-    var isMoreThanMaxAllowCharactersCount:Bool = true//当前状态是否超过了最大个数
+    //input status
+    var isExceedThanMaxAllowCharactersCount:Bool = true
     var currentText:String{
         return self.textView.text
     }
     
     //place holder
-    //tip: place holder文字大小最好和输入框内文字的大小相同
+    //tip: place holder font size perfers to be text view font size
     var placeHolderText:NSAttributedString?{
         didSet{
             self.placeHolderLabel.attributedText = placeHolderText
         }
     }
     
-    //提示文字属性
+    //property about tip label
     let tipLabelHeight:CGFloat = 20.0
-
-    //内部控件
+    private var tipLabelHeightConstraint:NSLayoutConstraint?
+    
+    //internal controls
     private(set) var textView:UITextView;
     private(set) var tipLabel:UILabel;
     private var placeHolderLabel:TIVLabel;
 
-    init(frame: CGRect, _delegate:TIVTextViewDelegate) {
-        //初始化
-        delegate = _delegate;
+    override init(frame: CGRect) {
+        //initialize property
         textView = UITextView(frame:CGRectMake(0, 0, frame.size.width, frame.size.height))
         tipLabel = UILabel(frame: CGRectMake(0, frame.size.height, frame.size.width, 0))
         placeHolderLabel = TIVLabel(frame: CGRectMake(4, 4, frame.size.width, 20))
         
         super.init(frame: frame)
+        self.setupSubViews()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        //initialize property
+        let frame = CGRectMake(0, 0, 20, 20);
+        textView = UITextView(frame:CGRectMake(0, 0, frame.size.width, frame.size.height))
+        tipLabel = UILabel(frame: CGRectMake(0, frame.size.height, frame.size.width, 0))
+        placeHolderLabel = TIVLabel(frame: CGRectMake(4, 4, frame.size.width, 20))
         
-        //配置子页面信息
+        super.init(coder: aDecoder)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.setupSubViews()
+    }
+
+    private func setupSubViews()
+    {
+        //config subview
         textView.delegate = self;
         textView.textColor = UIColor.blackColor();
         
@@ -65,10 +84,31 @@ class TIVTextView: UIView {
         self.addSubview(textView)
         self.addSubview(tipLabel)
         self.addSubview(placeHolderLabel)
-    }
+        
+        
+        //add constraint 
+        textView.translatesAutoresizingMaskIntoConstraints = false;
+        tipLabel.translatesAutoresizingMaskIntoConstraints = false;
+        placeHolderLabel.translatesAutoresizingMaskIntoConstraints = false;
+        
+        //config textView
+        self.addConstraint(NSLayoutConstraint(item: textView, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Leading, multiplier: 1, constant: 0))
+        self.addConstraint(NSLayoutConstraint(item: textView, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Trailing, multiplier: 1, constant: 0))
+        self.addConstraint(NSLayoutConstraint(item: textView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: 0))
+        self.addConstraint(NSLayoutConstraint(item: textView, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: self.tipLabel, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: 0))
 
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        //config tipLabel
+        self.addConstraint(NSLayoutConstraint(item: tipLabel, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Leading, multiplier: 1, constant: 0))
+        self.addConstraint(NSLayoutConstraint(item: tipLabel, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Trailing, multiplier: 1, constant: 0))
+        self.addConstraint(NSLayoutConstraint(item: tipLabel, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Bottom, multiplier: 1, constant: 0))
+        tipLabelHeightConstraint = NSLayoutConstraint(item: tipLabel, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 0);
+        self.addConstraint(tipLabelHeightConstraint!)
+
+        //config placeHolderLabel
+        self.addConstraint(NSLayoutConstraint(item: placeHolderLabel, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Leading, multiplier: 1, constant: 4))
+        self.addConstraint(NSLayoutConstraint(item: placeHolderLabel, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Trailing, multiplier: 1, constant: 4))
+        self.addConstraint(NSLayoutConstraint(item: placeHolderLabel, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: 4))
+        self.addConstraint(NSLayoutConstraint(item: placeHolderLabel, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 20))
     }
 }
 
@@ -76,41 +116,54 @@ extension TIVTextView: UITextViewDelegate{
     
     //UITextViewDelegate
     func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n"{
+            if let temp = self.delegate{
+                return temp.textView(shouldEndEditing: self)
+            }
+        }
         return true
     }
     
     func textViewDidBeginEditing(textView: UITextView) {
-        self.delegate.textView(didBeginEditing: self)
+        if let temp = self.delegate{
+            return  temp.textView(didBeginEditing: self)
+        }
     }
     
     func textViewDidEndEditing(textView: UITextView) {
-        self.delegate.textView(didEndEditing: self)
+        if let temp = self.delegate{
+            temp.textView(didEndEditing: self)
+        }
     }
     
     func textViewShouldEndEditing(textView: UITextView) -> Bool {
-        return self.delegate.textView(shouldEndEditing: self)
+        if let temp = self.delegate{
+            return temp.textView(shouldEndEditing: self)
+        }
+        return true
     }
     
     func textViewDidChange(textView: UITextView) {
         dispatch_async(calQueue) {[weak self] () -> Void in
             
-            if let weakSelf = self{
+            if let weakSelf = self, temp = weakSelf.delegate{
                 
-                let info:(tip:String, isMore:Bool) = weakSelf.delegate.textView(didChanged: weakSelf)
-                weakSelf.isMoreThanMaxAllowCharactersCount = info.isMore
+                let info:(tip:String, isMore:Bool) = temp.textView(didChanged: weakSelf)
                 
                 dispatch_async(dispatch_get_main_queue(), { () -> Void in
                     
-                    let width = weakSelf.frame.size.width
-                    let height = weakSelf.frame.size.height
-                    
-                    if weakSelf.isMoreThanMaxAllowCharactersCount{
-                        weakSelf.textView.frame = CGRectMake(0, 0, width, height - weakSelf.tipLabelHeight)
-                        weakSelf.tipLabel.frame = CGRectMake(0, height - weakSelf.tipLabelHeight, width, weakSelf.tipLabelHeight)
+                    weakSelf.isExceedThanMaxAllowCharactersCount = info.isMore
+
+                    if weakSelf.isExceedThanMaxAllowCharactersCount{
+                        weakSelf.tipLabelHeightConstraint?.constant = 20;
+                        weakSelf.needsUpdateConstraints()
+                        
                         weakSelf.tipLabel.hidden = false;
                         weakSelf.tipLabel.text = info.tip;
                     }else{
-                        weakSelf.textView.frame = CGRectMake(0, 0, width, height)
+                        weakSelf.tipLabelHeightConstraint?.constant = 0;
+                        weakSelf.needsUpdateConstraints()
+                        
                         weakSelf.tipLabel.hidden = true;
                     }
                     
--- TextInputViewGroup/ViewController.swift
+++ TextInputViewGroup/ViewController.swift
@@ -10,23 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: TIVTextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let textView:TIVTextView = TIVTextView(frame: CGRectMake(100, 100, 200, 200), _delegate: self)
+        textView.delegate = self;
         textView.backgroundColor = UIColor.brownColor()
         textView.placeHolderText = NSAttributedString(string: "请输入评论， 不超过20个字", attributes: [NSFontAttributeName: UIFont.systemFontOfSize(12), NSForegroundColorAttributeName: UIColor.whiteColor()])
         
         textView.textView.backgroundColor = UIColor.grayColor()
         textView.tipLabel.backgroundColor = UIColor.orangeColor()
-
-        self.view.addSubview(textView)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
     }
 }
 
@@ -37,31 +41,31 @@ extension ViewController:TIVTextViewDelegate{
         get{
             return 20
         }
-    }//允许输入字符的最大个数
+    }
     
-    func textView(didBeginEditing textView:TIVTextView)//开始输入
+    func textView(didBeginEditing textView:TIVTextView)
     {
         print("text view did begin editing")
     }
     
-    func textView(didEndEditing textView:TIVTextView)//结束输入
+    func textView(didEndEditing textView:TIVTextView)
     {
         print("text view did end editing")
     }
     
-    func textView(shouldEndEditing  textView:TIVTextView) -> Bool//键盘是否该收起
+    func textView(shouldEndEditing  textView:TIVTextView) -> Bool
     {
         return true
     }
     
-    func textView(didChanged textView:TIVTextView) -> (String, Bool)//文字发生变化时，调用方返回提示文字及当前文字个数。此方法被放入了子线程调用，调用方不用担心主线程被卡顿
+    func textView(didChanged textView:TIVTextView) -> (String, Bool)
     {
         let maxCount = self.maxAllowCharactersCount
         let currentTextCount = textView.currentText.characters.count
         
-        let moreCount = currentTextCount - maxCount;
-        if moreCount > 0{//超过字符限制
-            return ("超过\(moreCount)个文字", true)
+        let exccedCount = currentTextCount - maxCount;
+        if exccedCount > 0{//excced the max
+            return ("超过\(exccedCount)个文字", true)
         }else{
             return ("", false)
         }
